40d36c588020eaaa940468be256c17981dcab101
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-08-05 19:47:52 +0200

    Adds user model

---
+++ src/models/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
