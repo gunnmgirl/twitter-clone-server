f15572ca3abfdc480f69c33bf02b0d5c95ca3c53
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-08-05 19:20:06 +0200

    Connects to a server

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+//import routes
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+//app.use("/posts", postRoutes);
+
+app.use((error, req, res, next) => {
+  console.log("In error middleware ", error);
+  res.status(error.statusCode).send(error.data);
+});
+
+mongoose
+  .connect(process.env.CONNECTION_STRING, { useUnifiedTopology: true })
+  .then((result) => {
+    console.log("Connected to a server!");
+    app.listen(4000);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
