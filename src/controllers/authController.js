1054a55d42c7752f7aea269d029795079233515c
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-08-14 10:25:09 +0200

    Adds jwt authentication

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,6 @@
 import { validationResult } from "express-validator/check";
 import bcrypt from "bcryptjs";
-//import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 import User from "../Models/userModel";
 
@@ -28,4 +28,32 @@ async function signup(req, res, next) {
   }
 }
 
-export default { signup };
+async function login(req, res, next) {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      const error = new Error("A user with this email could not be found!");
+      error.statusCode = 401;
+      throw error;
+    }
+    const isEqual = await bcrypt.compare(req.body.password, user.password);
+    if (!isEqual) {
+      const error = new Error("You entered a wrong password!");
+      error.statusCode = 401;
+      throw error;
+    }
+    const token = jwt.sign(
+      { email: user.email, userId: user._id.toString() },
+      process.env.SECRET,
+      { expiresIn: "1h" }
+    );
+    res.status(200).send({ token, userId: user._id.toString() });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+}
+
+export default { signup, login };
