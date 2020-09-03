1328c0c5cc0cab5c194bbee0fea9e2a86f71b646
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-09-03 13:16:20 +0200

    Links post to a user that created it

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,4 +1,5 @@
 import Post from "../models/postModel";
+import User from "../models/userModel";
 
 async function createPost(req, res, next) {
   try {
@@ -8,6 +9,9 @@ async function createPost(req, res, next) {
       downvotes: 0,
       creator: req.userId,
     });
+    const user = await User.findById(req.userId);
+    user.posts.push(post);
+    await user.save();
     res.status(200).send(post);
   } catch (error) {
     return res.status(400).send(error);
